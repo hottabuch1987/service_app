50a3cef234b713a693d96c4dec3023c12acd2fda
hottabych <varvar1987a@mail.ru>
2023-01-11 15:03:26 +0300

    Day 5 - 1. celery - task правильный вариант

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -1,12 +1,14 @@
 from celery import shared_task
+from django.db.models import F
 
 
 @shared_task
 def set_price(subscription_id):
     from services.models import Subscription
 
-    subscription = Subscription.objects.get(id=subscription_id)
-    new_price = (subscription.servise.full_price -
-                 subscription.servise.full_price * subscription.plan.discount_percent / 100)
-    subscription.price = new_price
-    subscription.save(save_model=False)
+    subscription = Subscription.objects.filter(id=subscription_id).annotate(
+                annotated_price=F('service__full_price') -
+                  F('service__full_price') * F('plan__discount_percent') / 100).first()
+
+    subscription.price = subscription.annotated_price
+    subscription.save()
--- service/services/views.py
+++ service/services/views.py
@@ -15,8 +15,7 @@ class SubscriptionView(ReadOnlyModelViewSet):
                  queryset=Client.objects.all().select_related('user').only('company_name',
                                                                            'user__email'))
     )
-    # .annotate(price=F('service__full_price') -
-    #                  F('service__full_price') * F('plan__discount_percent') / 100)
+
     serializer_class = SubscriptionSerializer
 
     def list(self, request, *args, **kwargs):
