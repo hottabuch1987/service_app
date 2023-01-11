b631cc5c7b69b65d0d14a2765a296b0dbff81316
hottabych <varvar1987a@mail.ru>
2023-01-11 10:11:49 +0300

    Day 3 - 4. оптимизация методом annotate на уровне базы

--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -11,7 +11,9 @@ class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
         'plan',
 
-        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name',
+        Prefetch('client',
+                 queryset=Client.objects.all().select_related('user').only('company_name',
                                                               'user__email'))
-    )
+        ).annotate(price=F('service__full_price') -
+                     F('service__full_price') * F('plan__discount_percent') / 100)
     serializer_class = SubscriptionSerializer
