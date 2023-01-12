0999e2019ccea06303cad7cc504d24bd7eb97e15
hottabych <varvar1987a@mail.ru>
2023-01-12 09:52:25 +0300

    Day 5 - 3. celery - параллельные таски, transaction

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -1,7 +1,9 @@
+import datetime
 import time
 
 from celery import shared_task
 from celery_singleton import Singleton
+from django.db import transaction
 from django.db.models import F
 
 
@@ -9,11 +11,27 @@ from django.db.models import F
 def set_price(subscription_id):
     from services.models import Subscription
 
-    time.sleep(5)
 
-    subscription = Subscription.objects.filter(id=subscription_id).annotate(
+    with transaction.atomic():
+        time.sleep(5)
+
+        subscription = Subscription.objects.select_for_update().filter(id=subscription_id).annotate(
                 annotated_price=F('service__full_price') -
                   F('service__full_price') * F('plan__discount_percent') / 100).first()
 
-    subscription.price = subscription.annotated_price
-    subscription.save()
+        time.sleep(20)
+        subscription.price = subscription.annotated_price
+        subscription.save()
+
+
+
+
+@shared_task(base=Singleton)
+def set_comment(subscription_id):
+    from services.models import Subscription
+    with transaction.atomic():
+        subscription = Subscription.objects.objects.select_for_update().get(id=subscription_id)
+
+        time.sleep(27)
+        subscription.comment = str(datetime.datetime.now())
+        subscription.save()
